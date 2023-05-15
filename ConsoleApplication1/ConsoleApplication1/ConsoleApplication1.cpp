79146bbc378679fb427d591ceddbcd70fedeb408
Иван Боровков <88048424+JohnnyJa@users.noreply.github.com>
2023-05-16 01:12:32 +0300

    Smth2

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace  std;
+
+
+int main(int argc, char* argv[])
+{
+    int a = 10, b = 21, c = 17;
+        
+        if(a&b|~c)
+            cout << (a&b|~c);
+        return 0;
+    return 0;
+}
